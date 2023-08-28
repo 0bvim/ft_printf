16e91cc771534131c322d445c52ed10a983a7139
Vinicius Pereira <niviciusdev@gmail.com>
2023-08-28 14:37:20 -0300

    fix: included some enums 	Changes to be committed: 	modified:   includes/ft_printf.h

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: vde-frei <vde-frei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/26 05:07:17 by vde-frei          #+#    #+#             */
-/*   Updated: 2023/08/26 09:52:29 by vde-frei         ###   ########.fr       */
+/*   Updated: 2023/08/28 14:37:17 by vde-frei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,39 @@
 # define FT_PRINTF_H
 
 /* libs */
+# include <sys/types.h>
 # include <stdlib.h>
 # include <unistd.h>
 # include <stdarg.h>
 # include "../libraries/libft/includes/libft.h"
 
+/* defines */
+# define HEX_LW "0123456789abcdef"
+# define HEX_UP "0123456789ABCDEF"
+# define OCT_BASE "01234567"
+# define DEC_BASE "0123456789"
+# define NULL_PTR "(nil)"
+# define NULL_STR "(null)"
+
+# define HEX_L_PREFIX "0x"
+# define HEX_U_PREFIX "0X"
+# define OCT_PREFIX "0"
+
+/* enum and or structs */
+
+typedef enum e_specifiers
+{
+	c = 'c',
+	s = 's',
+	p = 'p',
+	i = 'i',
+	u = 'u',
+	x = 'x',
+	X = 'X',
+	d = 'd',
+	percent = '%',
+}		t_flag;
 /* functions */
-int	ft_printf(const char *s, ...);
+int	ft_printf(const char *form, ...);
 
 #endif /* FT_PRINTF */
