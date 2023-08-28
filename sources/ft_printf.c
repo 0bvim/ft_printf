0f17a736e10a754fcd699b5f74ecd135000f6ec0
Vinicius Pereira <niviciusdev@gmail.com>
2023-08-28 00:52:48 -0300

    fix: updated main file.  Changes to be committed: 	modified:   ft_printf.c

--- sources/ft_printf.c
+++ sources/ft_printf.c
@@ -6,13 +6,28 @@
 /*   By: vde-frei <vde-frei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/26 05:15:07 by vde-frei          #+#    #+#             */
-/*   Updated: 2023/08/26 09:53:35 by vde-frei         ###   ########.fr       */
+/*   Updated: 2023/08/28 00:52:43 by vde-frei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
 
-int	main()
+int	ft_printf(const char *sym, ...)
 {
+	va_list	ap;
+	int	len;
+	char	*str;
+
+	va_start(ap, sym);
+	len = 0;
+	str = NULL;
+	while (*sym)
+	{
+		if (*sym == 37)
+		{
+			sym++;
+			//create function to put sym
+		}
+	}
 	return (0);
 }
